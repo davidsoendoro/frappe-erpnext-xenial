3a1047d3102c7861838dc138e57099310fcc2d7f
david soendoro <david.soendoro@gmail.com>
2017-03-07 00:28:58 +0700

    Make change for xenial

--- setup_frappe.sh
+++ setup_frappe.sh
@@ -209,19 +209,22 @@ install_packages() {
 		run_cmd bash -c "curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -"
 		run_cmd sudo apt-get update
 		run_cmd sudo apt-get install -y --allow-unauthenticated python-dev python-setuptools build-essential python-mysqldb git \
-			ntp vim screen htop mariadb-server mariadb-common libmariadbclient-dev \
+			ntp vim screen htop mariadb-server mariadb-common \
 			libxslt1.1 libxslt1-dev redis-server libssl-dev libcrypto++-dev postfix nginx \
 			supervisor python-pip fontconfig libxrender1 libxext6 xfonts-75dpi xfonts-base nodejs
 
 		if [ $OS_VER == "precise" ]; then
 			run_cmd sudo apt-get install -y --allow-unauthenticated libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev \
-				liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
+				liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk libmariadbclient-dev
 		elif [ $OS_VER == "8" ]; then
 			run_cmd sudo apt-get install -y --allow-unauthenticated libtiff5-dev libjpeg62-turbo-dev zlib1g-dev libfreetype6-dev \
-				liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk npm
+				liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk npm libmariadbclient-dev
+        elif [ $OS_VER == "xenial" ]; then
+			run_cmd sudo apt-get install -y --allow-unauthenticated libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev \
+				liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk libmariadb-client-lgpl-dev
 		else
 			run_cmd sudo apt-get install -y --allow-unauthenticated libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev \
-				liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
+				liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk libmariadbclient-dev
 		fi
 
 		echo "Installing wkhtmltopdf"
